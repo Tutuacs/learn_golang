2616fae06018769483f070232f4cdbac6c6ffa4b
Arthur_Silva <arthursilva.rs1@gmail.com>
2024-09-18 17:25:51 -0300

    add error.go

--- projects/todo/pkg/utils/error.go
+++ projects/todo/pkg/utils/error.go
@@ -1,1 +1,14 @@
 package utils
+
+import (
+	"fmt"
+
+	"todo-app/pkg/logs"
+)
+
+func ErrorPanic(err error) {
+	if err != nil {
+		logs.ErrorLog(fmt.Sprintf("ocorreu um erro inesperado: %v", err))
+		panic(err)
+	}
+}
